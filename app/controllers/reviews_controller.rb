131d1b81abf6909cc35077ba0f3e319e386f399e
LeeAnne Ward <leeanneward@Dallass-iMac.local>
2016-06-08 14:03:20 -0500

    write a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,14 +16,12 @@ class ReviewsController < ApplicationController
   # POST /reviews.json
   def create
     @review = Review.new(review_params)
-
     @review.user_id = current_user.id
-
     @review.restaurant_id = @restaurant.id
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to root_path, notice: 'Review was successfully created.' }
+        format.html { redirect_to @restaurant, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
